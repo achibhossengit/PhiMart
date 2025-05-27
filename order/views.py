78988d2147b020cede1e6e815547ef6d6c8e7d16
achibhossengit <mail.achibhossen@gmail.com>
2025-05-27 07:53:11 +0600

    modify create method of CartViewsets

--- order/views.py
+++ order/views.py
@@ -8,6 +8,7 @@ from order.permissions import IsCartOwnerUser
 from rest_framework.decorators import action
 from order.services import OrderService
 from rest_framework.response import Response
+from rest_framework import status
 
 # not inherite ListModelMixins
 class CartViewSets(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -16,6 +17,13 @@ class CartViewSets(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gene
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        existing_cart = Cart.objects.filter(user=request.user).first()
+        if existing_cart:
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().create(request, *args, **kwargs)
 
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
